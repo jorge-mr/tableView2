8afda0edbc4c720a7ead73b52204eb11b6e5551f
Jorge MR <jorge_mr@icloud.com>
2018-01-10 21:59:41 -0600

    Initial Commit

---
+++ tableView2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableView2
+//
+//  Created by Jorge MR on 10/01/18.
+//  Copyright © 2018 jorge_a_mtz_r. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
